46a4b804e4c9a9c42c2c5f17fca3737984639faf
Sudhanshu Suvasiya <159800404+ANeuronI@users.noreply.github.com>
2025-04-12 02:33:12 +0530

    Update Activity_5c.py

--- Activity_5c.py
+++ Activity_5c.py
@@ -8,7 +8,7 @@ Open_api_key = os.getenv("OPENAI_API_KEY")
 client = OpenAI(api_key=Open_api_key)
 
 
-speech_file_path = Path(__file__).parent / "speech.mp3"
+speech_file_path = Path(__file__).parent / "speech.mp3" # this will define speech file path in the parent directory (i.e where your code is).
 
 with client.audio.speech.with_streaming_response.create(
     model="gpt-4o-mini-tts",
@@ -16,4 +16,4 @@ with client.audio.speech.with_streaming_response.create(
     input="Today is a wonderful day to build something people love!",
     instructions="Speak in a cheerful and positive tone.",
 ) as response:
-    response.stream_to_file(speech_file_path)
+    response.stream_to_file(speech_file_path) # it is saving the audio in the speech.mp3 created in your parent directory.
